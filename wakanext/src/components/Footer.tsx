ea718ef9984f20de6dc48678dce63b774f724d18
waka320 <yukiwakamatsu6a@gmail.com>
2024-12-03 23:43:30 +0900

    made top page

---
+++ wakanext/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="w-full bg-gray-900 text-white p-4">
+            © {new Date().getFullYear()} Yuki
+        </footer>
+    )
+}
---
+++ wakanext/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <header className="w-full bg-gray-100 p-4">
+        a
+      </header>
+    )
+  }
