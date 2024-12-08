ae7c34b1fe107cdc4236a465e8af138ed3f2997e
waka320 <yukiwakamatsu6a@gmail.com>
2024-12-08 23:26:14 +0900

    adjust dependacies

--- wakanext/src/@types/gray-matter.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'gray-matter' {
-    interface GrayMatterFile<T = string> {
-        data: T;
-        content: string;
-    }
-
-    function matter(input: string): GrayMatterFile;
-    export = matter;
-}
