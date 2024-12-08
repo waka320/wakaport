081967a75fc8ad99a115d11420e469c6638541ab
waka320 <yukiwakamatsu6a@gmail.com>
2024-12-08 16:33:02 +0900

    devided in components

--- wakanext/src/components/home/content.tsx
+++ wakanext/src/components/home/content.tsx
@@ -1,32 +1,31 @@
-import Image from "next/image";
-import { Button } from "../ui/button";
+import { Card } from "./card";
 export default function Content() {
     return (
-        <div className="content-background">
-            <h2>About</h2>
-            <div className="flex container">
-                <div className="flex-[1]">
-                    <Image
-                        src="/images/profile.jpg"
-                        alt="プロフィール画像"
-                        width={150}
-                        height={150}
-                        className="rounded-lg border-4 border-black"
-                    />
-                </div>
-                <div className="flex-[2] flex flex-col justify-between">
-                    <div>
-                        <p>名古屋大学情報学部人間・社会情報学科の３年生です。</p>
-                        <p>研究領域は、岐阜県高山市における、観光×ICTです。</p>
-                        <p>また、Webのフロントエンドを始めとした技術に関心を持っています。</p>
-                    </div>
-                    <div className="mt-2 self-start">
-                        <Button href="/about">
-                            私について
-                        </Button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <>
+            <Card
+                imageSrc="/images/profile.jpg"
+                title="About"
+                paragraphs={[
+                    "名古屋大学情報学部人間・社会情報学科の３年生です。",
+                    "研究領域は、岐阜県高山市における、観光×ICTです。",
+                    "また、Webのフロントエンドを始めとした技術に関心を持っています。"
+                ]}
+                buttonText="私について"
+                buttonHref="/about"
+            />
+            <Card
+                imageSrc="/images/profile.jpg"
+                title="Products"
+                paragraphs={[
+                    "TypeScript, React, Next.js",
+                    "TailwindCSS, SASS",
+                    "フロントエンド開発に特化したスキルセット"
+                ]}
+                buttonText="やったこと"
+                buttonHref="/products"
+            />
+        </>
+
+
     )
 }
