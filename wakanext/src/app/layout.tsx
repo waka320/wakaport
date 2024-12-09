615392f46cf33fb370187f9175ad3ddd0d5a8ffc
waka320 <yukiwakamatsu6a@gmail.com>
2024-12-09 14:56:41 +0900

    adjust header

--- wakanext/src/app/layout.tsx
+++ wakanext/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '@/components/header'
+import Footer from '@/components/footer'
 import "@/styles/globals.scss";
 
 const geistSans = localFont({
--- wakanext/src/components/Header.tsx
+++ wakanext/src/components/Header.tsx
@@ -4,7 +4,9 @@ export default function Header() {
   return (
     <header className="w-full  bg-white shadow-md p-4 flex justify-between items-center">
       <div className="flex items-center">
-        <h1 className="my-auto text-xl font-bold">Wakaport.</h1>
+        <Link href="/" >
+          <h1 className="my-auto text-xl font-bold">Wakaport.</h1>
+        </Link>
       </div>
 
       <nav className="flex space-x-4">
