5947a4bd8be75255a4b80552f804dafe6d0f1b25
waka320 <yukiwakamatsu6a@gmail.com>
2025-02-17 01:11:44 +0900

    fix/changed props and promise

--- wakanext/src/app/(pages)/news/[slug]/page.tsx
+++ wakanext/src/app/(pages)/news/[slug]/page.tsx
@@ -11,12 +11,13 @@ export async function generateStaticParams() {
     }))
 }
 
-export default async function ArticleDetailPage(
-    props: {
-        params: Promise<{ slug: string }>
-    }
-) {
-    const params = await props.params;
+type PageProps = {
+    params: { slug: string }
+}
+
+export default async function ArticleDetailPage({
+    params
+}: PageProps) {
     try {
         const article = getArticleBySlug(params.slug)
 
@@ -50,14 +51,11 @@ export default async function ArticleDetailPage(
     }
 }
 
-export async function generateMetadata(
-    props: {
-        params: Promise<{ slug: string }>
-    }
-): Promise<Metadata> {
-    const params = await props.params;
+export async function generateMetadata({
+    params
+}: PageProps): Promise<Metadata> {
     try {
-        const article = getArticleBySlug(params.slug);
+        const article = getArticleBySlug(params.slug)
 
         return {
             title: article.title,
