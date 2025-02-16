d230bd4007862a4787eec2a156baf3fb7c79bc58
waka320 <yukiwakamatsu6a@gmail.com>
2025-02-17 00:53:40 +0900

    fix/adjust git config

--- wakanext/next.config.ts
+++ wakanext/next.config.ts
@@ -1,10 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   sassOptions: {
     implementation: 'sass-embedded',
     includePaths: ['./src/styles']
   },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
