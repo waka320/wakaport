0fc27293ce768ed9619c0fbf58a34543dac8a2f6
waka320 <yukiwakamatsu6a@gmail.com>
2025-02-17 01:03:33 +0900

    fix/modded code

--- wakanext/src/app/(pages)/news/[slug]/page.tsx
+++ wakanext/src/app/(pages)/news/[slug]/page.tsx
@@ -11,11 +11,12 @@ export async function generateStaticParams() {
     }))
 }
 
-export default async function ArticleDetailPage({
-    params
-}: {
-    params: { slug: string }
-}) {
+export default async function ArticleDetailPage(
+    props: {
+        params: Promise<{ slug: string }>
+    }
+) {
+    const params = await props.params;
     try {
         const article = getArticleBySlug(params.slug)
 
@@ -49,11 +50,12 @@ export default async function ArticleDetailPage({
     }
 }
 
-export async function generateMetadata({
-    params
-}: {
-    params: { slug: string }
-}): Promise<Metadata> {
+export async function generateMetadata(
+    props: {
+        params: Promise<{ slug: string }>
+    }
+): Promise<Metadata> {
+    const params = await props.params;
     try {
         const article = getArticleBySlug(params.slug);
 
