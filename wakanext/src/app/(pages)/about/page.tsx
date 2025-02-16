3b5674c2897911e6feb68358d77abaf29f5d3a45
waka320 <yukiwakamatsu6a@gmail.com>
2025-02-17 01:46:22 +0900

    fix/erased things that isn't used

--- wakanext/src/app/(pages)/about/page.tsx
+++ wakanext/src/app/(pages)/about/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import React, { useState } from 'react';
 import IntroductionSection from '@/components/about/IntroductionSection';
 import AffiliationsSection from '@/components/about/AffiliationsSection';
 import SkillsSection from '@/components/about/SkillsSection';
