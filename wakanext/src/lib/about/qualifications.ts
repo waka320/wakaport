ad3dcec2dfdd20718408f184c25d4a38d960c5c5
Waka.Y <104607401+waka320@users.noreply.github.com>
2025-10-13 16:49:43 +0900

    feat: awards

--- wakanext/src/lib/about/qualifications.ts
+++ wakanext/src/lib/about/qualifications.ts
@@ -1,6 +1,7 @@
 export const qualifications = {
     title: "資格・受賞",
     items: [
+        "JPHACKS2025 PASONAスポンサー賞 2025年10月 受賞",
         "技育CAMP 2025 vol.7 努力賞 2025年8月 受賞",
         "TOEIC LR 890点 2025年 取得",
         "JPHACKS2024 AwardDay進出、三菱重工スポンサー賞、NTTドコモスポンサー賞 2024年受賞",
